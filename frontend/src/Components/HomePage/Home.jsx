e66ba0645786c9e3f0020be0d3f06c69d2d70118
SriVats18 <vatsnagwan0318@gmail.com>
2024-04-15 18:26:53 +0530

    Added our logo to the home page of website..

--- frontend/src/Components/HomePage/Home.jsx
+++ frontend/src/Components/HomePage/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <>
         <div id='main' className='bg-[url("https://i.pinimg.com/564x/61/c9/e0/61c9e0ba50bb0bdded1db82f761d912c.jpg")] bg-[length:630px_800px] sm:bg-[length:1550px_800px] bg-no-repeat bg-fixed text-white'>
             {/* NAVBAR */}
-            <div id='navbar' className='sticky top-0 z-20 rounded-xl backdrop-blur-2xl hover:bg-[#08387B] duration-1000'>
+            <nav id='navbar' className='sticky top-0 z-20 rounded-xl backdrop-blur-2xl hover:bg-[#08387B] duration-1000'>
                 <ul className='flex justify-around p-4 text-3xl font-bree font-extrabold'>
                     <li className='cursor-pointer hover:scale-150 hover:text-orange-700 active:text-orange-400 duration-700'><a href="#main">Home</a></li>
                     <li className='cursor-pointer hover:scale-150 hover:text-orange-700 active:text-orange-400 duration-700'><a href="#about">About</a></li>
@@ -13,15 +13,16 @@ const Home = () => {
                     <li className='cursor-pointer hover:scale-150 hover:text-orange-700 active:text-orange-400 duration-700'><a href="#content">Analyzers</a></li>
                     <li className='cursor-pointer hover:scale-150 hover:text-orange-700 active:text-orange-400 duration-700'><a href="#contact">Contact Us</a></li>
                 </ul>
-            </div>
-
-            {/* <div className='flex justify-center items-center p-10 pb-0 mb-0'>
-                <a href="#main"><img src="./Assets/logo.png" alt="Logo" className='rounded-full size-48 cursor-zoom-in' /></a>
-            </div> */}
+            </nav>
 
             {/* MAIN */}
-            <div className='grid place-content-center h-screen w-full'>
-                <h1 className='font-averia uppercase text-center text-7xl font-bold md:text-[150px]'>Chat & comment analyzer</h1>
+            <div className='flex flex-col gap-5 -mt-10 justify-center items-center h-screen w-full'>
+                <div>
+                    <img src="../images/logo_2.png" alt="Logo" className='h-52 rounded-full' />
+                </div>
+                <div>
+                    <h1 className='font-averia uppercase text-center text-7xl font-bold md:text-[150px]'>Chat & comment analyzer</h1>
+                </div>
             </div>
         </div>
         </>

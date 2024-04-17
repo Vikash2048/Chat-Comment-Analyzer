677da3609da86dc528318fdd830bacf82a52f4b4
SriVats18 <vatsnagwan0318@gmail.com>
2024-04-17 19:51:42 +0530

    Merged the current changes and incoming changes..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,8 @@ import { YoutubeDataContext } from "./Context/YoutubeDataContext"
 import Fpage from './Pages/Fpage';
 import Whatsapp from "./Analyzer's/Whatsapp"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
 import Guide from './Components/Guide';
-=======
 import YoutubeAnalyzer from './youtube_analysis/YoutubeAnalyzer';
->>>>>>> 2193ae825c211012e5236dec0018d7aca369d4ed
 
 
 function App() {
@@ -34,25 +31,23 @@ function App() {
   return (
     <Router>
       <>
-<<<<<<< HEAD
-        <DataContext.Provider value={{ userlist, setUserList, stats, setStats, monthlytimeline, setMonthlyTimeline, dailytimeline, setDailyTimeline, mostbusyday, setMostBusyDay, mostbusymonth, setMostBusyMonth, file, setFile, mostbusyuser, setMostBusyUser, listbusyuser, setListBusyUser }}>
+        {/* <DataContext.Provider value={{ userlist, setUserList, stats, setStats, monthlytimeline, setMonthlyTimeline, dailytimeline, setDailyTimeline, mostbusyday, setMostBusyDay, mostbusymonth, setMostBusyMonth, file, setFile, mostbusyuser, setMostBusyUser, listbusyuser, setListBusyUser }}>
           <Routes>
             <Route path='/' exact Component={Fpage} />
             <Route path='/whatsappAnalyzer' Component={Whatsapp} />
             <Route path='/Guide' Component={Guide} />
           </Routes>
-        </DataContext.Provider>
-=======
+        </DataContext.Provider> */}
         <YoutubeDataContext.Provider value={{videoDetail, setVideoDetail, sa, setSA, positiveComment, setPositiveComment, negetiveComment, setNegetiveComment, neutralComment, setNeutralComment, topComment, setTopComment}}>
           <DataContext.Provider value={{ userlist, setUserList, stats, setStats, monthlytimeline, setMonthlyTimeline, dailytimeline, setDailyTimeline, mostbusyday, setMostBusyDay, mostbusymonth, setMostBusyMonth, file, setFile, mostbusyuser, setMostBusyUser, listbusyuser, setListBusyUser }}>
             <Routes>
               <Route path='/' exact Component={Fpage} />
               <Route path='/whatsappAnalyzer' Component={Whatsapp} />
+              <Route path='/Guide' Component={Guide} />
               <Route path='/youtubeAnalyzer' Component={YoutubeAnalyzer} />
             </Routes>
           </DataContext.Provider>
         </YoutubeDataContext.Provider>
->>>>>>> 2193ae825c211012e5236dec0018d7aca369d4ed
       </>
     </Router>
 

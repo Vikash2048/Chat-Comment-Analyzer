58915e8ff4e855fdd7c5f50084d30010b3f23551
SriVats18 <vatsnagwan0318@gmail.com>
2024-04-18 23:58:23 +0530

    Fixed styling in content page..

--- frontend/src/Components/HomePage/Content.jsx
+++ frontend/src/Components/HomePage/Content.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Content = () => {
   // State to handle hover effect
-  const [isHovering, setIsHovering] = useState(false);
+  const [wHovering, setWHovering] = useState(false);
+  const [yHovering, setYHovering] = useState(false);
 
   return (
     <>
@@ -16,10 +17,10 @@ const Content = () => {
         <div className='flex h-screen w-full flex-row items-center justify-around p-4 font-averia font-extrabold text-2xl md:text-5xl'>
 
           {/* WhatsApp Section.. */}
-          <div className='grid place-content-center h-screen w-full hover:bg-slate-800/65 hover:text-orange-700 hover:scale-125 active:bg-orange-400 duration-1000' onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+          <div className='grid place-content-center h-screen w-full hover:bg-slate-800/65 hover:text-orange-700 hover:scale-125 active:bg-orange-400 duration-1000' onMouseEnter={() => setWHovering(true)} onMouseLeave={() => setWHovering(false)}>
             <Link to="/whatsappAnalyzer">
-              <h3 className='text-center hover:duration-1000'>
-                {isHovering ? 
+              <h3 className='text-center'>
+                {wHovering ? 
                   <img src="../images/whatsapp_logo.jpg" alt="WhatsApp image" className="w-40 h-40 rounded-xl" /> :
                   'WhatsApp'
                 }
@@ -28,10 +29,10 @@ const Content = () => {
           </div>
           
           {/* YouTube Section.. */}
-          <div className='grid place-content-center h-screen w-full hover:bg-slate-800/65 hover:text-orange-700 hover:scale-125 active:bg-orange-400 duration-1000' onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+          <div className='grid place-content-center h-screen w-full hover:bg-slate-800/65 hover:text-orange-700 hover:scale-125 active:bg-orange-400 duration-1000' onMouseEnter={() => setYHovering(true)} onMouseLeave={() => setYHovering(false)}>
             <Link to="/youtubeAnalyzer">
-              <h3 className='text-center hover:duration-1000'>
-                {isHovering ? 
+              <h3 className='text-center'>
+                {yHovering ? 
                   <img src="../images/youtube logo.jpg" alt="YouTube image" className="w-40 h-40 rounded-xl" /> :
                   'YouTube'
                 }
